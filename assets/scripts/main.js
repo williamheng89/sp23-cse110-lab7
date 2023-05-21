251afe4473b221ae6ba81a87656afe1a59ffbfff
williamheng89 <williamheng89@gmail.com>
2023-05-21 12:25:04 -0700

    finished lab

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,14 +45,27 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
+  if ('serviceWorker' in navigator){
+    // B2. TODO - Listen for the 'load' event on the window object.
+    window.addEventListener('load', async() => {
+      // Steps B3-B6 will be *inside* the event listener's function created in B2
+      // B3. TODO - Register './sw.js' as a service worker (The MDN article
+      //            "Using Service Workers" will help you here)
+      try {
+        const registration = await navigator.serviceWorker.register("sw.js", {
+          scope: ".",
+        });
+        // B4. TODO - Once the service worker has been successfully registered, console
+        //            log that it was successful.
+        if (registration.active)
+          console.log("Service worker successfully registered")
+      } catch (error) {
+        // B5. TODO - In the event that the service worker registration fails, console
+        //            log that it has failed.
+        console.error(`Registration failed with ${error}`);
+      }
+    });
+  }
   // STEPS B6 ONWARDS WILL BE IN /sw.js
 }
 
@@ -68,10 +81,13 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  if (localStorage.getItem('recipes'))
+    return JSON.parse(localStorage.getItem('recipes'));
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  let fetchRecipes = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
@@ -81,25 +97,41 @@ async function getRecipes() {
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
   /**************************/
-  // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
-  //            declared above
-  // A5. TODO - Since we are going to be dealing with asynchronous code, create
-  //            a try / catch block. A6-A9 will be in the try portion, A10-A11
-  //            will be in the catch portion.
-  // A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
-  //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
-  //            you must either use "await fetch(...)" or "fetch.then(...)". This
-  //            function is using the async keyword so we recommend "await"
-  // A7. TODO - For each fetch response, retrieve the JSON from it using .json().
-  //            NOTE: .json() is ALSO asynchronous, so you will need to use
-  //            "await" again
-  // A8. TODO - Add the new recipe to the recipes array
-  // A9. TODO - Check to see if you have finished retrieving all of the recipes,
-  //            if you have, then save the recipes to storage using the function
-  //            we have provided. Then, pass the recipes array to the Promise's
-  //            resolve() method.
-  // A10. TODO - Log any errors from catch using console.error
-  // A11. TODO - Pass any errors to the Promise's reject() function
+  return new Promise ((resolve, reject) => {
+    // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
+    //            declared above
+    RECIPE_URLS.forEach(async recipe => {
+      // A5. TODO - Since we are going to be dealing with asynchronous code, create
+      //            a try / catch block. A6-A9 will be in the try portion, A10-A11
+      //            will be in the catch portion.
+      try {
+          // A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
+          //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
+          //            you must either use "await fetch(...)" or "fetch.then(...)". This
+          //            function is using the async keyword so we recommend "await"
+          const url = await fetch(recipe);
+          // A7. TODO - For each fetch response, retrieve the JSON from it using .json().
+          //            NOTE: .json() is ALSO asynchronous, so you will need to use
+          //            "await" again
+          const newRecipe = await url.json();
+          // A8. TODO - Add the new recipe to the recipes array
+          fetchRecipes.push(newRecipe);
+          // A9. TODO - Check to see if you have finished retrieving all of the recipes,
+          //            if you have, then save the recipes to storage using the function
+          //            we have provided. Then, pass the recipes array to the Promise's
+          //            resolve() method.
+          if (fetchRecipes.length == RECIPE_URLS.length){
+            saveRecipesToStorage(fetchRecipes);
+            resolve(fetchRecipes);
+          }
+      } catch (error) {
+        // A10. TODO - Log any errors from catch using console.error
+        console.log(error);
+        // A11. TODO - Pass any errors to the Promise's reject() function
+        reject(error);
+      }
+    });  
+  });
 }
 
 /**
